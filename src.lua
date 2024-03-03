93eaea6ec73d0bb415c1ca806feb973c01046ccc
Grayy12 <graysoncarter690@yahoo.com>
2024-03-02 23:26:22 -0500

    Update src.lua

--- src.lua
+++ src.lua
@@ -10,18 +10,6 @@ local function CheckFile(filepath)
 	end
 end
 
-local function Hash(str: string)
-	if syn.crypt.hash then
-		return syn.crypt.hash(str)
-	elseif sha384_hash then
-		return sha384_hash(str)
-	elseif fluxus.crypt.hash then
-		return fluxus.crypt.hash(str)
-	else
-		return str
-	end
-end
-
 function SaveSys.Init(Folder: string?, File: string)
 	local self = {}
 	local exists = true
@@ -84,35 +72,6 @@ function SaveSys.Init(Folder: string?, File: string)
 		end
 
 		return game:GetService("HttpService"):JSONDecode(Data)
-	end
-
-	function self:SetupKey(key: string)
-		local self2 = {}
-		self2._Folder = self._Folder
-
-		if not syn.crypt.hash and not sha384_hash and not fluxus.crypt.hash then
-			warn("Encryption is not supported on this executor or not known, key will be visible")
-		end
-		if not CheckFile(("%s/%s"):format(self._Folder, "Key.txt")) and not CheckFile("Key.txt") then
-			writefile(self._Folder ~= "" and ("%s/%s"):format(self._Folder, "Key.txt") or "Key.txt", Hash(key))
-		end
-
-		function self2:CheckKey(key: string): boolean
-			if not CheckFile(("%s/%s"):format(self._Folder, "Key.txt")) and not CheckFile("Key.txt") then
-				warn("Key file does not exist")
-				return false
-			end
-
-			local Key = readfile(self._Folder ~= "" and ("%s/%s"):format(self._Folder, "Key.txt") or "Key.txt")
-
-			if Hash(key) == Key then
-				return true
-			else
-				return false
-			end
-		end
-
-		return self2
 	end
 
 	return self, exists
